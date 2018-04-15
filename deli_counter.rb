74feafae09d6ce4b9ac184b0e55318c09e9cd0e8
ERIC KELLY <eakelly220@gmail.com>
2018-04-15 19:03:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,5 +23,14 @@ end
 def take_a_number(line, name)
     line << name
     puts"Welcome, #{name}. You are number #{line.length} in line."
-    
+end 
+
+def now_serving(line) 
+ 
+  if line.length == 0 
+    puts "There is nobody waiting to be served!"
+  elsif 
+    while 0 < line.length
+    puts "Now serving #{line[0]}"
+    line.shift
 end 
